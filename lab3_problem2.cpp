6b53ca51cdd56da6c090854dac1a930638cefeba
nomanislam222 <113965562+nomanislam222@users.noreply.github.com>
2022-10-13 12:40:57 +0600

    Update lab3_problem2.cpp

--- lab3_problem2.cpp
+++ lab3_problem2.cpp
@@ -1,32 +1,33 @@
 #include<iostream>
-
 using namespace std;
 
-int main()
-{
-char message[100], ch;
-int i, key;
-cout << "Enter a message to encrypt: ";
-cin.getline(message, 100);
-cout << "Enter key: ";
-cin >> key;
-for(i = 0; message[i] != '\0'; ++i){
-ch = message[i];
+void encode(string s, int j){
+char ch;
+for(int i = 0; s[i] != '\0'; ++i){
+ch = s[i];
 if(ch >= 'a' && ch <= 'z'){
-ch = ch + key;
+ch = ch + j;
 if(ch > 'z'){
 ch = ch - 'z' + 'a' - 1;
 }
-message[i] = ch;
+s[i] = ch;
 }
 else if(ch >= 'A' && ch <= 'Z'){
-ch = ch + key;
+ch = ch + j;
 if(ch > 'Z'){
 ch = ch - 'Z' + 'A' - 1;
 }
-message[i] = ch;
+s[i] = ch;
 }
 }
-cout << "Encrypted message: " << message;
+cout << "Converted String: " << s;
+}
+
+
+
+int main()
+{
+cout << "Sample String(s): I am a student" << endl << "Sample Integer(j): 2"<<endl;
+encode("I am a student",2);
 return 0;
 }
